c4c20877ab71ea7071268c6220f8c1c0075a0ebe
serophiom <serophiom@gmail.com>
2021-08-03 23:33:31 +0300

    Update Phonebook.js

--- src/Phonebook/Phonebook.js
+++ src/Phonebook/Phonebook.js
@@ -5,9 +5,12 @@ import { v4 as uuidv4 } from 'uuid';
 import Filter from '../Filter/Filter';
 import './Phonebook.css';
 
-export default function Phonebook({ onSubmit }) {
+export default function Phonebook() {
     const [contacts, setContacts] = useState(
-        JSON.parse(window.localStorage.getItem('contacts')) ?? [],
+        JSON.parse(window.localStorage.getItem('contacts')) ?? [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+                {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+                {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+                {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
     );
     const [filter, setFilter] = useState('');
 
@@ -49,7 +52,7 @@ export default function Phonebook({ onSubmit }) {
     }, [contacts]);
 
     const toLowerCase = filter.toLowerCase();
-    const showContacts = contacts.filter(contact =>
+    const showContacts = contacts.filter((contact) =>
          contact.name.toLowerCase().includes(toLowerCase)
     );
 
