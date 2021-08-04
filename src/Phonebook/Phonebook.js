a4731fc009bb39a9f434a86151d2724a17473875
serophiom <serophiom@gmail.com>
2021-08-04 11:22:55 +0300

    Update Phonebook.js

--- src/Phonebook/Phonebook.js
+++ src/Phonebook/Phonebook.js
@@ -14,23 +14,13 @@ export default function Phonebook() {
     );
     const [filter, setFilter] = useState('');
 
-    // state = {
-    //     contacts: [
-    //         {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    //         {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    //         {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    //         {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    //     ],
-    //     filter: ''
-    // }
-
-    const formSubmitHandler = ({ name, number }) => {
+    const formSubmitHandler = ( name, number ) => {
         const alreadyAddedContact = contacts.find(contact => contact.name === name);
         if (alreadyAddedContact) {
             alert(`${alreadyAddedContact.name} is already in contacts`);
             return;
         }
-        setContacts (...contacts, { id: uuidv4(), name, number });
+        setContacts ([...contacts, { id: uuidv4(), name, number }]);
     };
 
     const handleFilter = value => {
@@ -41,18 +31,12 @@ export default function Phonebook() {
         setContacts(contacts.filter(contact => contact.id !== contactId));
     };
 
-    // useEffect(() => {
-    //     if (localStorage.getItem('contacts')?.length > 0) {
-    //         setContacts(JSON.parse(localStorage.getItem('contacts')));
-    //     }
-    // }, []);
-
     useEffect (() => {
         window.localStorage.setItem('contacts', JSON.stringify(contacts));
     }, [contacts]);
 
     const toLowerCase = filter.toLowerCase();
-    const showContacts = contacts.filter((contact) =>
+    const showContacts = contacts.filter(contact =>
          contact.name.toLowerCase().includes(toLowerCase)
     );
 
@@ -66,71 +50,3 @@ export default function Phonebook() {
         </div>
     );
 }
-
-// class Phonebook extends Component {
-//     state = {
-//         contacts: [
-//             {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-//             {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-//             {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-//             {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-//         ],
-//         filter: ''
-//     }
-
-//     formSubmitHandler = ({ name, number }) => {
-//         const { contacts } = this.state;
-//         const alreadyAddedContact = contacts.find(contact => contact.name === name);
-//         if (alreadyAddedContact) {
-//             alert(`${alreadyAddedContact.name} is already in contacts`);
-//             return
-//         }
-//         this.setState(({ contacts }) => ({
-//             contacts: [...contacts, { id: uuidv4(), name, number }]
-//         }))
-//     };
-
-//     handleFilter = (value) => {
-//         this.setState({ filter: value });
-//     };
-
-//     deleteContact = (contactId) => {
-//         this.setState(prevState => ({
-//             contacts: prevState.contacts.filter(contact => contact.id !== contactId)
-//         }))
-//     };
-
-//     componentDidMount() {
-//         if (localStorage.getItem('contacts')?.length > 0) {
-//             this.setState({
-//                 contacts: JSON.parse(localStorage.getItem('contacts'))
-//             });
-//         }
-//     }
-
-//     componentDidUpdate(prevProps, prevState) {
-//         if (this.state.contacts !== prevState.contacts) {
-//             localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//         }
-//     }
-
-//     render() {
-//         const { contacts, filter } = this.state;
-//         const toLowerCase = filter.toLowerCase();
-//         const showContacts = contacts.filter(contact =>
-//             contact.name.toLowerCase().includes(toLowerCase)
-//         );
-
-//         return (
-//             <div className="phonebook">
-//                 <h1>Phonebook</h1>
-//                 <FormAddContacts onSubmit={this.formSubmitHandler}/>
-//                 <h2>Contacts</h2>
-//                 <Filter change={this.handleFilter} filter={filter} contacts={contacts}/>
-//                 <Contacts contacts={contacts} showContacts={showContacts} onDeleteContact={this.deleteContact}/>
-//             </div>
-//         );
-//     }
-// }
-
-// export default Phonebook;
